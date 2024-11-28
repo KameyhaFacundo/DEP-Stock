e11a69edba83a63caf8ea3477cd71d6ce2c14d2f
Agustin Lobo <loboagus10@gmail.com>
2024-11-28 12:07:06 -0300

    Update Menu.jsx
    Modificada la imagen del navbar para que no hayan scroles horizontales en dispositivos pequeños

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logoNavbar from '../../assets/img/header.jpg'
+import logoNavbarResponsive from '../../assets/img/header-responsive-SM.png';
 
 const Menu = () => {
     return (
       <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand as={Link} to={'/'}>
-          <img src={logoNavbar} alt="Direccion Estadistica de la provincia de Tucuman" className='logo-navbar' />
-        </Navbar.Brand>
-      </Container>
+        <Container className='mt-0'>
+          <Navbar.Brand as={Link} to={'/'} className='d-flex justify-content-center d-xl-block d-md-none'>
+            <img src={logoNavbar} alt="Direccion Estadistica de la provincia de Tucuman" className='logo-navbar w-100' />
+          </Navbar.Brand>
+          <Navbar.Brand as={Link} to={'/'} className='d-flex justify-content-center d-none d-md-block d-sm-block d-xl-none'>
+            <img src={logoNavbarResponsive} alt="Direccion Estadistica de la provincia de Tucuman" className='logo-navbar w-100' />
+          </Navbar.Brand>
+        </Container>
     </Navbar>
     );
 };
