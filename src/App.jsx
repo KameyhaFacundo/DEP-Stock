7c07c9fc943165c2634f938a874c97f727b3b57e
Agustin Lobo <loboagus10@gmail.com>
2024-11-28 12:05:19 -0300

    Update App.jsx
    Agregado el componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Menu from './components/common/Menu'
+import Footer from './components/common/Footer';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Menu></Menu>
+      <div className='mainSection'> xdsadsa</div>
+      <Footer></Footer>
     </BrowserRouter>
   )
 }
