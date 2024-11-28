d9367e58be33f0d3f171337724ea48414904a8b4
Agustin Lobo <loboagus10@gmail.com>
2024-11-28 13:03:44 -0300

    Update App.jsx
    Agregada la llamada al componente Login el cual es el primero que se mostrara al tener el enlace "/". Se agrego una ruta que luego servira para controlar el acceso

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Menu from './components/common/Menu'
 import Footer from './components/common/Footer';
+import Login from './components/Views/Login';
+import { useState } from 'react';
 
 function App() {
 
+  const usuarioEnLocalStorage = JSON.parse(sessionStorage.getItem('user')) || {}; 
+  const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioEnLocalStorage);
+
+  const logout = () =>
+  { 
+    sessionStorage.removeItem('user');
+    setUsuarioLogueado({});
+  }
   return (
     <BrowserRouter>
       <Menu></Menu>
-      <div className='mainSection'> xdsadsa</div>
+        <Routes>
+          <Route exact path='/' element={<Login usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}></Login>}></Route>
+        </Routes>
       <Footer></Footer>
     </BrowserRouter>
   )
