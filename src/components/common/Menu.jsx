b05d35682bc3fbc525106b3f28b72f862a52fb5d
Agustin Lobo <loboagus10@gmail.com>
2024-12-05 12:41:31 -0300

    Update Menu.jsx
    Modificada la imagen del navbar para que redirija a diferentes rutas en caso de estar logueado el usuario o no

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -5,13 +5,19 @@ import logoNavbar from '../../assets/img/header.jpg'
 import logoNavbarResponsive from '../../assets/img/header-responsive-SM.png';
 
 const Menu = () => {
+  const usuarioLogueado = JSON.parse(sessionStorage.getItem('user')) || null;
+  let link = '/';
+  if(usuarioLogueado)
+  {
+    link = '/home/';
+  }
     return (
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container className='mt-0'>
-          <Navbar.Brand as={Link} to={'/'} className='d-flex justify-content-center d-xl-block d-md-none'>
+          <Navbar.Brand as={Link} to={link} className='d-flex justify-content-center d-xl-block d-md-none'>
             <img src={logoNavbar} alt="Direccion Estadistica de la provincia de Tucuman" className='logo-navbar w-100' />
           </Navbar.Brand>
-          <Navbar.Brand as={Link} to={'/'} className='d-flex justify-content-center d-none d-md-block d-sm-block d-xl-none'>
+          <Navbar.Brand as={Link} to={link} className='d-flex justify-content-center d-none d-md-block d-sm-block d-xl-none'>
             <img src={logoNavbarResponsive} alt="Direccion Estadistica de la provincia de Tucuman" className='logo-navbar w-100' />
           </Navbar.Brand>
         </Container>
