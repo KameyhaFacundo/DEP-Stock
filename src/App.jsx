cffbee7269b2504db9a64b0534af35b7c98a0c5f
Agustin Lobo <loboagus10@gmail.com>
2024-12-05 12:21:10 -0300

    Update App.jsx
    Agregada la seccion para las rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Menu from './components/common/Menu'
 import Footer from './components/common/Footer';
 import Login from './components/Views/Login';
 import { useState } from 'react';
+import RutasProtegidas from './components/Routes/RutasProtegidas';
+import RutasAdministrador from './components/Routes/RutasAdministrador';
 
 function App() {
 
@@ -21,6 +23,11 @@ function App() {
       <Menu></Menu>
         <Routes>
           <Route exact path='/' element={<Login usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}></Login>}></Route>
+          <Route path='/home' element={
+            <RutasProtegidas>
+              <RutasAdministrador></RutasAdministrador>
+            </RutasProtegidas>
+          }></Route>
         </Routes>
       <Footer></Footer>
     </BrowserRouter>
