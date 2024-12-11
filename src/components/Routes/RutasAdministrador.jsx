ec9445eaa5aad3221e7e456b1b4128d91db97f6b
Agustin Lobo <loboagus10@gmail.com>
2024-12-11 10:12:18 -0300

    Update RutasAdministrador.jsx
    Agregada la ruta protegida para ver la tabla de oficinas

--- src/components/Routes/RutasAdministrador.jsx
+++ src/components/Routes/RutasAdministrador.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Inicio from '../Views/Inicio';
+import Oficinas from '../Views/Oficinas/Oficinas';
 
 const RutasAdministrador = ({logout}) => {
     return (
-        <Routes>
+        <Routes> 
             <Route 
             exact path='/'
             element={<Inicio logout={logout}></Inicio>}></Route>
+            <Route 
+            exact path='/oficinas'
+            element={<Oficinas logout={logout}></Oficinas>}
+            ></Route>
         </Routes>
     );
 };
