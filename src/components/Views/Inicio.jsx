67da18286e535433b8bc1396614f04ed1aa11721
Facundo Kameyha <kameyhafacundo@gmail.com>
2024-12-12 01:48:30 -0300

    Agrego el componente Movimiento y su estilo

--- src/components/Views/Inicio.jsx
+++ src/components/Views/Inicio.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import MenuInicio from './MenuInicio/MenuInicio';
+import React from "react";
+import MenuInicio from "./MenuInicio/MenuInicio";
+import Movimiento from "./Movimientos/Movimiento";
 
-const Inicio = ({logout}) => {
-    return (
-        <div className='mainSection'>
-            <MenuInicio logout={logout}/>
-            <h1>Bienvenido a la pagina prncipal :)</h1>
-        </div>
-        
-    );
+const Inicio = ({ logout }) => {
+  return (
+    <div className="mainSection">
+      <MenuInicio logout={logout} />
+      {/* <h1>Bienvenido a la pagina prncipal :)</h1> */}
+      <Movimiento></Movimiento>
+    </div>
+  );
 };
 
 export default Inicio;
--- src/components/Views/MenuInicio/MenuInicio.jsx
+++ src/components/Views/MenuInicio/MenuInicio.jsx
@@ -1,27 +1,29 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { PersonBadgeFill } from 'react-bootstrap-icons';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { PersonBadgeFill } from "react-bootstrap-icons";
 
-const MenuInicio = ({logout}) => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-            <Navbar.Brand href="#home">DEP-Stock Manager</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                <Nav.Link href="#miPerfil">
-                    <PersonBadgeFill className='me-2' size={30}></PersonBadgeFill>
-                    Ver Perfil
-                </Nav.Link>
-                <Nav.Link href="#link" onClick={logout}>Cerrar Sesión</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+const MenuInicio = ({ logout }) => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#miPerfil">
+              <PersonBadgeFill className="me-2" size={30}></PersonBadgeFill>
+              Ver Perfil
+            </Nav.Link>
+            <Nav.Link href="#link" onClick={logout}>
+              Cerrar Sesión
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default MenuInicio;
