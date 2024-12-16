b29d8588866ed22fbd6945c6ad950ba6acd6475b
Agustin978 <loboagus10@gmail.com>
2024-12-16 12:13:05 -0300

    Creada la funcion para traer los movimientos de una oficina pasando el id de dicha oficina

--- src/components/helpers/oficinas.js
+++ src/components/helpers/oficinas.js
@@ -13,3 +13,18 @@ export const fetchCentros = async () =>
         throw error
     }
 }
+
+//Para traer los movimientos de una oficina en particular
+export const fetchMovimientosDeCentro = async (idCentro) =>
+{
+    try
+    {
+        const response = await fetch(`${API_BASE_URL}/MovimientosPorOficina.php?idCentro=${idCentro}`);
+        const data = await response.json();
+        return data.data || [];
+    }catch(error)
+    {
+        console.error("Error fetching movimientos de centros: ", error);
+        throw error;
+    }
+}
