e5312651555f9c219d854947d4ba37b7bd46130b
Agustin978 <loboagus10@gmail.com>
2024-12-16 12:18:21 -0300

    Modificado el item para mostrar un boton de movimientos, agregados los componentes para ocultar y mostrar el modal donde se ven los movimientos de la oficina

--- src/components/Views/Oficinas/ItemOficina.jsx
+++ src/components/Views/Oficinas/ItemOficina.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import ModalMovimientosOficina from './ModalMovimientosOficina';
 
-const ItemOficina = ({id, nombre}) => {
+const ItemOficina = ({id, nombre, key}) => {
+    const [showModalMovimientos, setShowModalMovimientos] = useState(false);
+    const handleCloseModalMovimientos = () => setShowModalMovimientos(false);
+    const handleShowModalMovimientos = () => setShowModalMovimientos(true);
+
     return (
-        <tr>
+        <tr key={key}>
             <td>{id}</td>
             <td>{nombre}</td>
             <td>
                 <Container>
                     <Row>
-                        <Col><Button>Movimientos</Button></Col>
+                        <Col><Button onClick={handleShowModalMovimientos}>Movimientos</Button></Col>
+                        {
+                            showModalMovimientos && <ModalMovimientosOficina show={showModalMovimientos} handleClose={handleCloseModalMovimientos} idOficina={id}></ModalMovimientosOficina>
+                        }
                     </Row>
                 </Container>
             </td>
