cc746154bd256b726a40fcaee471b833c12aaf01
Agustin978 <loboagus10@gmail.com>
2024-12-16 12:16:50 -0300

    Modificados los titulos

--- src/components/Views/Oficinas/Oficinas.jsx
+++ src/components/Views/Oficinas/Oficinas.jsx
@@ -14,33 +14,30 @@ const Oficinas = ({logout}) => {
             try {
                 const oficinas = await fetchCentros(); // Espera a que se resuelva la promesa
                 setData({ centros: oficinas }); // Establece el estado correctamente
-                console.log(data.centros);
             } catch (error) {
                 console.log("Error al cargar los datos: ", error);
             }
         };
         loadData();
     }, []);
-
-    console.log(data.centros);
     return (
         <div className='mainSection'>
             <MenuInicio logout={{logout}}></MenuInicio>
             <div className="d-flex justify-content-center align-items-center mt-5">
-                <h1 className='display-4'>Oficinas</h1>
+                <h1 className='display-4'>Centro de Costos</h1>
             </div>
             <Container className='py-2'>
                 <Table responsive striped bordered hover className='text-center'>
                     <thead>
                         <tr>
-                            <th>Id Oficina</th>
+                            <th>Id Centro</th>
                             <th>Nombre</th>
                             <th>Opciones</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {data.centros.map((centro) => (
-                            <ItemOficina id={centro.IdCentro} nombre={centro.Centro}></ItemOficina>
+                        {data.centros.map((centro, index) => (
+                            <ItemOficina id={centro.IdCentro} nombre={centro.Centro} key={index}></ItemOficina>
                         ))}
                     </tbody>
                 </Table>
